25e5630da22fa69fa3fc0f8fc4a4e9e7c7e60406
Aaron Gnan <aaron.gnan@student.kit.edu>
2022-12-28 03:07:54 +0100

    .

---
+++ config.js
@@ -0,0 +1,3 @@
+const config = {
+    API_KEY: 'AhONH9tljwepwehs2_rdrECD_SKisRVggoUEJbgALFhFlPCobF32-UmsffYV2s-g',
+};
---
+++ script.js
@@ -0,0 +1,20 @@
+function fireLaser() {
+    const laser = new Audio('laser.mp3');
+    laser.play().then(() => null);
+}
+
+function getLocation() {
+    const x = document.getElementById('location');
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+                x.innerHTML = 'Latitude: ' + latitude + '<br>Longitude: ' + longitude + '<br>Link: ' +
+                    '<a href=\"https://www.google.de/maps/@' + latitude + ',' + longitude + ',16z\">Google Maps</a>';
+            },
+            () => x.innerHTML = 'Geolocation not available');
+    } else {
+        x.innerHTML = 'Geolocation is not supported by this browser.';
+    }
+}
